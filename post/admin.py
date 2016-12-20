3350f28ec3dbedb2fef97ebb2fc755e3639b02bc
KirillMoskvin <kirill.moskvin@mail.ru>
2016-12-20 18:55:03 +0300

    Added admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from post.models import Post, Comments
 
 
 # Register your models here.
+
+class PostInline(admin.StackedInline):
+	model = Comments
+	extra = 2
+
+
+
+class PostAdmin(admin.ModelAdmin):
+	fields = ["post_title", "post_text", "post_date"]
+	inlines = [PostInline]
+	list_filter = ['post_date']
+#	post_display = ['article_title', 'article_date']
+
+admin.site.register(Post, PostAdmin)
