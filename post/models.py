58416dc4d8d1649d49e472c43b8e1182fa64c942
KirillMoskvin <kirill.moskvin@mail.ru>
2016-12-20 17:34:45 +0300

    added commsents

--- post/models.py
+++ post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    class Meta():
+        db_table = "post"
+
+    post_title = models.CharField(max_length = 200)
+    post_text = models.TextField()
+    post_date = models.DateTimeField()
+    post_likes = models.IntegerField(default=0)
+
+class Comments(models.Model):
+    class Meta():
+        db_table = 'сomments'
+
+    comments_text = models.TextField()
+    comments_article = models.ForeignKey(Post)
+
